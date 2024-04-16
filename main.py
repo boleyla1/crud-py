0073772ff00e82332110fbd9daa3d9b3fecc1df3
boleyla1 <boleyla.mehrshad1@gmail.com>
2024-04-16 20:39:01 +0330

    test2 crud

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
+
 from tkinter import *
-Window = Tk()
+import tkinter as tk
+from tkinter import ttk
+
+def btn_func():
+    Entry_text = Entry.get()
+    lable['text'] = Entry_text
+    print(lable['text'])
+
+# window
+Window = tk.Tk()
 Window.title('resgister')
 Window.geometry('700x700')
 Window.resizable(False, False)
 Window.config(bg='darkgray')
 
 
-Register = Button(Window, text='Register').place(x=100, y=100)
+string_var = tk.StringVar(value='your name')
 
 
+#
+lable = ttk.Label(Window, text='Entry Text', textvariable=string_var)
+lable.pack()
+Entry = ttk.Entry(Window, textvariable=string_var)
+Entry.pack()
+btn = ttk.Button(Window, text='hello', command=btn_func)
+btn.pack()
 
+# run window with loop
 Window.mainloop()
